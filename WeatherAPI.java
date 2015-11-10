2cfcb1be18479246fd662534cd2a98596b3ce8e7
DarkAce65 <tvdood99@gmail.com>
2015-11-10 17:28:35 -0500

    Add getDataFromURL function

--- WeatherAPI.java
+++ WeatherAPI.java
@@ -1,3 +1,6 @@
+import java.net.*;
+import java.io.*;
+
 public class WeatherAPI {
 	private String city;
 	private String state;
@@ -6,6 +9,20 @@ public class WeatherAPI {
 	public WeatherAPI() {
 		city = "";
 		state = "";
+	}
+
+	public String getDataFromURL(String queryURL) throws Exception {
+		URL requestURL = new URL(queryURL);
+		URLConnection connection = requestURL.openConnection();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+		String response = "";
+		String inputLine;
+		while((inputLine = reader.readLine()) != null) {
+			response += inputLine;
+		}
+
+		reader.close();
+		System.out.println(response);
 	}
 
 	public String getLatitudeLongitude(String query) {
