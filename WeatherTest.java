0c0d4ecdb00108e05ddfff95d7a036306dfa1f42
DarkAce65 <tvdood99@gmail.com>
2015-11-12 19:59:11 -0500

    Create WeatherTest.java

---
+++ WeatherTest.java
@@ -0,0 +1,7 @@
+public class WeatherTest {
+	public static void main(String[] args) {
+		WeatherObject weatherObject = new WeatherObject(42.658336, -71.136795);
+
+		System.out.println(weatherObject);
+	}
+}
