7c61ff6185c620d0ec68ca863b1e1b0c40fc993f
mollybear <bear@zoombear.com>
2015-11-16 17:39:06 -0500

    fix index error line 27

--- WeatherForecast.java
+++ WeatherForecast.java
@@ -24,7 +24,7 @@ public class WeatherForecast extends JFrame implements ActionListener
 	    String text = location.getText();
 			try{
 			  String city = text.substring(0, text.indexOf(','));
-			  String state = text.substring(text.indexOf(',') +  1, text.length());
+			  String state = text.substring(text.indexOf(',') +  2, text.length());
 				weather.updateWeatherForecast(city, state);
 				today.setText(weather.toString());
 		 } catch (StringIndexOutOfBoundsException e){
